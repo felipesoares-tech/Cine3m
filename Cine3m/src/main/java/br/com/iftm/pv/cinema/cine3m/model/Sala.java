b1611014b02b1d0029d62d5d703ab7eb88717eb8
ElisabeteXavier <elisabetebx@gmail.com>
2023-05-13 11:50:45 -0300

    feat: logica das poltronas

--- Cine3m/src/main/java/br/com/iftm/pv/cinema/cine3m/model/Sala.java
+++ Cine3m/src/main/java/br/com/iftm/pv/cinema/cine3m/model/Sala.java
@@ -1,7 +1,7 @@
-
 package br.com.iftm.pv.cinema.cine3m.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,6 +9,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Sala {
+
     @EqualsAndHashCode.Include
     private String nome;
     private Integer capacidade;
@@ -18,13 +19,28 @@ public class Sala {
         this.nome = nome;
         this.capacidade = capacidade;
         this.poltronas = new ArrayList<Poltrona>(capacidade);
-        
+        int i = 0;
+
+        for (char fileira = 'A'; i < capacidade / 10; fileira++) {
+            for (int numero = 1; numero <= 10; numero++) {
+
+                String id = fileira + Integer.toString(numero);
+                Poltrona poltrona = new Poltrona(id);
+                poltronas.add(poltrona);
+
+            }
+            i++;
+        }
+        Iterator<Poltrona> it = poltronas.iterator();
+        while(it.hasNext()){
+            Poltrona poltrona = it.next();
+            System.out.println(poltrona.getId());
+        }
+
     }
-    
+
     public Sala(String nome) {
         this.nome = nome;
     }
-    
-    
-    
+
 }
